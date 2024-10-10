d177c3024b768380022de5077f5507dd5a226d10
Yordan Madzhunkov <ymadzhunkov@gmail.com>
2024-10-10 13:39:43 +0300

    Read private key from file in CMC

--- examples/cmc/src/lib.rs
+++ examples/cmc/src/lib.rs
@@ -10,6 +10,8 @@ use serde::Deserialize;
 use serde_json::Value;
 use url::Url;
 
+use std::fs;
+
 #[allow(dead_code)]
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -38,37 +40,6 @@ pub struct Status {
 #[serde(rename_all = "camelCase")]
 pub struct CmcData {
     id: i64,
-    // pub name: String,
-    // pub symbol: String,
-    // pub slug: String,
-    // #[serde(rename = "num_market_pairs")]
-    // pub num_market_pairs: Option<i64>,
-    // #[serde(rename = "date_added")]
-    // pub date_added: Option<String>,
-    // pub tags: Vec<Tag>,
-    // #[serde(rename = "max_supply")]
-    // pub max_supply: Option<i64>,
-    // #[serde(rename = "circulating_supply")]
-    // pub circulating_supply: Option<f64>,
-    // #[serde(rename = "total_supply")]
-    // pub total_supply: Option<f64>,
-    // #[serde(rename = "is_active")]
-    // pub is_active: Option<f64>,
-    // #[serde(rename = "infinite_supply")]
-    // pub infinite_supply: Option<bool>,
-    // pub platform: Value,
-    // #[serde(rename = "cmc_rank")]
-    // pub cmc_rank: Option<i64>,
-    // #[serde(rename = "is_fiat")]
-    // pub is_fiat: Option<i64>,
-    // #[serde(rename = "self_reported_circulating_supply")]
-    // pub self_reported_circulating_supply: Value,
-    // #[serde(rename = "self_reported_market_cap")]
-    // pub self_reported_market_cap: Value,
-    // #[serde(rename = "tvl_ratio")]
-    // pub tvl_ratio: Value,
-    // #[serde(rename = "last_updated")]
-    // pub last_updated: Option<String>,
     quote: HashMap<String, CmcValue>,
 }
 
@@ -86,37 +57,21 @@ pub struct Tag {
 #[serde(rename_all = "camelCase")]
 pub struct CmcValue {
     pub price: f64,
-    // #[serde(rename = "volume_24h")]
-    // pub volume_24h: Option<f64>,
-    // #[serde(rename = "volume_change_24h")]
-    // pub volume_change_24h: Option<f64>,
-    // #[serde(rename = "percent_change_1h")]
-    // pub percent_change_1h: Option<f64>,
-    // #[serde(rename = "percent_change_24h")]
-    // pub percent_change_24h: Option<f64>,
-    // #[serde(rename = "percent_change_7d")]
-    // pub percent_change_7d: Option<f64>,
-    // #[serde(rename = "percent_change_30d")]
-    // pub percent_change_30d: Option<f64>,
-    // #[serde(rename = "percent_change_60d")]
-    // pub percent_change_60d: Option<f64>,
-    // #[serde(rename = "percent_change_90d")]
-    // pub percent_change_90d: Option<f64>,
-    // #[serde(rename = "market_cap")]
-    // pub market_cap: Option<f64>,
-    // #[serde(rename = "market_cap_dominance")]
-    // pub market_cap_dominance: Option<f64>,
-    // #[serde(rename = "fully_diluted_market_cap")]
-    // pub fully_diluted_market_cap: Option<f64>,
-    // pub tvl: Value,
-    // #[serde(rename = "last_updated")]
-    // pub last_updated: Option<String>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 pub struct CmcResource {
     pub cmc_id: u64,
     pub cmc_quote: String,
+}
+
+fn trim_newline(s: &mut String) {
+    if s.ends_with('\n') {
+        s.pop();
+        if s.ends_with('\r') {
+            s.pop();
+        }
+    }
 }
 
 #[oracle_component]
@@ -137,11 +92,12 @@ async fn oracle_request(settings: Settings) -> Result<Payload> {
     let mut req = Request::builder();
     req.method(Method::Get);
     req.uri(url);
-    //TODO(adikov): Implement API key as capability of the reporter and add it to the header
-    //in the oracle trigger
 
     // Please provide your own API key until capabilities are implemented.
-    req.header("X-CMC_PRO_API_KEY", "");
+    let mut private_key: String = fs::read_to_string("/X-CMC_PRO_API_KEY")?;
+    trim_newline(&mut private_key);
+    // println!("Using private key for CoinMarketCap `{}`", &private_key);
+    req.header("X-CMC_PRO_API_KEY", &private_key);
     req.header("Accepts", "application/json");
 
     let req = req.build();
