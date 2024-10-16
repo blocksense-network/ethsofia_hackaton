0406803459923b63e7cffdd6ae55f79fd51175ea
Antoni Dikov <antoni.dikov@gmail.com>
2024-10-16 18:00:28 +0300

    feat(revolut-example): Fix Revolut example
    Co-authored-by: Antoni Dikov <antoni.dikov@gmail.com>

--- examples/revolut/src/lib.rs
+++ examples/revolut/src/lib.rs
@@ -18,31 +18,29 @@ struct Rate {
 
 #[oracle_component]
 async fn oracle_request(settings: Settings) -> Result<Payload> {
-    let data_feed = settings.data_feeds.first().unwrap();
-    let url = match data_feed.data.as_str() {
-        "USD/BTC" => Url::parse("https://www.revolut.com/api/quote/public/BTCUSD")?,
-        "USD/ETH" => Url::parse("https://www.revolut.com/api/quote/public/ETHUSD")?,
-        &_ => todo!(),
-    };
-    println!("URL - {}", url.as_str());
-    let mut req = Request::builder();
-    req.method(Method::Get);
-    req.uri(url);
-    req.header("user-agent", "*/*");
-    req.header("Accepts", "application/json");
-
-    let req = req.build();
-    let resp: Response = send(req).await?;
-
-    let body = resp.into_body();
-    let string = String::from_utf8(body).expect("Our bytes should be valid utf8");
-    let value: Rate = serde_json::from_str(&string).unwrap();
-
-    println!("{:?}", value);
     let mut payload: Payload = Payload::new();
-    payload.values.push(DataFeedResult {
-        id: data_feed.id.clone(),
-        value: DataFeedResultValue::Numerical(value.rate),
-    });
+    for data_feed in settings.data_feeds.iter() {
+        let url = Url::parse(format!("https://www.revolut.com/api/quote/public/{}", data_feed.data).as_str())?;
+        println!("URL - {}", url.as_str());
+        let mut req = Request::builder();
+        req.method(Method::Get);
+        req.uri(url);
+        req.header("user-agent", "*/*");
+        req.header("Accepts", "application/json");
+
+        let req = req.build();
+        let resp: Response = send(req).await?;
+
+        let body = resp.into_body();
+        let string = String::from_utf8(body).expect("Our bytes should be valid utf8");
+        let value: Rate = serde_json::from_str(&string).unwrap();
+
+        println!("{:?}", value);
+
+        payload.values.push(DataFeedResult {
+            id: data_feed.id.clone(),
+            value: DataFeedResultValue::Numerical(value.rate),
+        });
+    }
     Ok(payload)
 }
